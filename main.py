8d7cd002dd6ae70eb96263f63cba12a220736b3d
anton.concheiro <anton.concheiro@udc.es>
2021-04-13 22:04:21 +0200

    Creo que el encoder está hecho pero no estoy seguro porque no entiendo muy bien como funciona el gringo telingo mandingo

--- main.py
+++ main.py
@@ -7,28 +7,47 @@ def validate(line, length):
         if 0 > int(c) or int(c) > 2:
             raise ValueError('File has wrong format, the board should be composed of ones and zeroes')
 
-def translate(line, output):
-    pass
+def translate(line,numline, output):
+    pos=1    
+    for c in line:
+        output.write('h(cell('+str(numline)+','+str(pos)+'),')
+        output.write('off).\n') if int(c) == 0 else output.write('on).\n')
+        pos=pos+1
+
+def goal(output):
+    output.write('\n')
+    output.write('#program final.\n')
+    output.write('goal :- h(cell(X,Y),off), cell(X,Y).\n')
+    output.write(':- not goal.')
 
 def main():
 
-    if len(sys.argv) != 2:
-        raise ValueError('Please provide a text file to read from')
+    #if len(sys.argv) != 2:
+        #raise ValueError('Please provide a text file to read from')
 
     input = open(sys.argv[1], "r")
-    output = open(f'{sys.argv[1][:-4:]}.lp', 'w')
+    output = open(sys.argv[2], 'w+')
 
     lines = input.readlines()
 
     try: 
         length = int(lines[0])
+        output.write('%Domain description\n')
+        output.write('cell(1..'+str(length)+',1..'+str(length)+').\n')
+        output.write('action(tog(X,Y)) :- cell(X,Y).\n')
     except Exception as e:
         raise ValueError('File has wrong format, the size of the board must only be a positive integer')
 
-    for i in range(1, length):
+    output.write('\n')
+    output.write('#program initial.\n')
+    for i in range(1, length+1):
         line = lines[i].rstrip()
         validate(line, length)
-        translate(line, output)
+        translate(line,i, output)
+    goal(output)
+
+    output.write('\n')
+    output.write('#show o/1.')
 
 if __name__ =='__main__':
     main()
